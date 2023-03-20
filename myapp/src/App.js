3857febc91497e6cf5809ca4ec10e5876569598e
Jefff <gaobolun131622@gmail.com>
2023-03-20 14:48:17 -0500

    projectcreated

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Navbar} from './components'
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+export default App
